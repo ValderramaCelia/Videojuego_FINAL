0a263c5774cc5fdfcedb96ca63690d58b464c178
ValderramaCelia <cifersand@gmail.com>
2020-02-25 20:08:13 +0100

    Ultima actualización

---
+++ Assets/Script/AudioFuegote.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioFuegote : MonoBehaviour
+{
+    public AudioSource fuegoteS;
+    public AudioClip bfff;
+
+    public void FuegoteSound()
+    {
+
+        fuegoteS.PlayOneShot(bfff);
+    }
+
+    
+}
---
+++ Assets/Script/PassLevel3.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class PassLevel3 : MonoBehaviour
+{
+    int playerLayer;    //The layer the player game object is on
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the integer representation of the "Player" layer
+        playerLayer = LayerMask.NameToLayer("Player");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //If the collision wasn't with the player, exit
+        if (collision.gameObject.layer != playerLayer)
+            return;
+        SceneManager.LoadScene("Nivel 3");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
